49621c29e2b4195f401e75321cfbbdc484c0b347
Edson Cândido <edson.c.a.j@hotmail.com>
2025-06-16 23:46:57 -0300

    feat: config inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+import LoginPage from "./pages/login"
+
+function App() {
+
+  return (
+    <>
+    <LoginPage/>
+    </>
+  )
+}
+
+export default App
