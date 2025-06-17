98957b0b4266fb637582281612f6e539baf9befc
Edson Cândido <edson.c.a.j@hotmail.com>
2025-06-17 00:38:26 -0300

    feat: dashboard layout

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -3,11 +3,19 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useNavigate } from "react-router-dom"
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+
+    const navigate = useNavigate();
+
+  function handleLogin() {
+    localStorage.setItem("token", "fake-token");
+    navigate("/dashboard");
+  }
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
@@ -15,10 +23,10 @@ export function LoginForm({
           <form className="p-6 md:p-8">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
-                <p className="text-muted-foreground text-balance">
+                <h1 className="text-2xl font-bold">Bem vindo de volta 👋</h1>
+                {/* <p className="text-muted-foreground text-balance">
                   Login to your Acme Inc account
-                </p>
+                </p> */}
               </div>
               <div className="grid gap-3">
                 <Label htmlFor="email">Email</Label>
@@ -31,17 +39,17 @@ export function LoginForm({
               </div>
               <div className="grid gap-3">
                 <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
+                  <Label htmlFor="password">Senha</Label>
                   <a
                     href="#"
                     className="ml-auto text-sm underline-offset-2 hover:underline"
                   >
-                    Forgot your password?
+                    Esqueceu a senha?
                   </a>
                 </div>
                 <Input id="password" type="password" required />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" onClick={handleLogin}>
                 Login
               </Button>
             </div>
@@ -56,8 +64,7 @@ export function LoginForm({
         </CardContent>
       </Card>
       <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+       Desenvolvido por  <a href="#">&copy;ZukoSoftware</a> {new Date().getFullYear()}{" "}
       </div>
     </div>
   )
