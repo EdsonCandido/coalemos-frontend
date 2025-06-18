01b84ca3285304d01ba2202b1d048b8f764244c6
Edson Cândido <edson.c.a.j@hotmail.com>
2025-06-18 00:11:20 -0300

    feat: add SectionCards and Badge components for dashboard metrics display

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,27 @@
+import { SectionCards } from "@/components/dashboard/home/SectionCards"
+
 const DashboardHome = () => {
+    // return (
+    // <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+    {/* <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" /> */}
+
+    // </div>
+    // )
+
     return (
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
+        <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                    <SectionCards />
+                    <div className="px-4 lg:px-6">
+                        {/* <ChartAreaInteractive /> */}
+                    </div>
+                    {/* <DataTable data={data} /> */}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 export default DashboardHome
