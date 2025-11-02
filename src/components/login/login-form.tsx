bc75828604ec1646f8f27095b14c052d92126fce
Edson Cândido <edson.c.a.j@hotmail.com>
2025-11-02 10:41:52 -0300

    [refactor] remove console

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -36,7 +36,6 @@ export function LoginForm({
     const isSuccess = await login({ login: e.login, password: e.senha });
 
     if (isSuccess) navigate("/dashboard");
-    console.log(e);
   };
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
