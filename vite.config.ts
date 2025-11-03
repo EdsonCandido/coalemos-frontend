cdce1e1bc4041649fc908ed283b6463d3c6dd8d0
Edson Cândido <edson.c.a.j@hotmail.com>
2025-11-03 00:30:15 -0300

    [refactor]

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig, loadEnv, } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
-import compression from 'vite-plugin-compression';
-// https://vite.dev/config/
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import compression from "vite-plugin-compression";
+import path from "path";
 
 export default ({ mode }: { mode: string }) => {
-
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [react(), tsconfigPaths(),
-    compression({
-      algorithm: 'gzip',
-    }),
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      compression({
+        algorithm: "gzip",
+      }),
     ],
-    base: "/template-dashboard",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    base: "/",
     server: {
       open: true,
       watch: {
@@ -22,30 +28,22 @@ export default ({ mode }: { mode: string }) => {
       },
     },
     build: {
-      outDir: 'build',
+      outDir: "build",
       rollupOptions: {
         treeshake: false,
-        // output: {
-        //   manualChunks(id) {
-        //     if (id.includes('node_modules')) {
-        //       return 'vendor';
-        //     }
-        //   },
-        // },
-        // output: {
-        //   manualChunks: {
-        //     lodash: globalVendorPackages
-        //   }
-        // }
         output: {
           manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            if (id.includes("node_modules")) {
+              return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
             }
-          }
-        }
+          },
+        },
       },
-      minify: 'esbuild',
+      minify: "esbuild",
     },
-  })
+  });
 };
