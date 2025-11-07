539d13213a44bec92d930ec09ee5018453940795
Edson Cândido <edson.c.a.j@hotmail.com>
2025-11-06 22:20:26 -0300

    [feat] create CRUD for users

--- src/components/admin/Usuario/ListUsers/index.tsx
+++ src/components/admin/Usuario/ListUsers/index.tsx
@@ -1,79 +1,201 @@
-import { Stack, Text } from "@chakra-ui/react";
-
+import { Flex, Stack, Text } from "@chakra-ui/react";
 import { DataGrid } from "devextreme-react";
-import { Column, Pager, Paging, SearchPanel, Selection, Button as ButtonGrid } from "devextreme-react/data-grid";
-import { dateFormat, formatCPF } from "../../../../utils/mask";
-import Loading from "../../../Loading";
-import { User } from "../../../../types/AuthContextData";
-
-type dataFormat = User & { index: number };
+import {
+  Column,
+  Pager,
+  Paging,
+  SearchPanel,
+  Selection,
+} from "devextreme-react/data-grid";
+import { FaCheckCircle, FaSearch, FaTimesCircle } from "react-icons/fa";
+import { IconButton } from "rsuite";
+import type { tUsuarios } from "@/types/types";
+import Loading from "@/components/Loading";
+import { dateFormat } from "@/utils/mask";
+import { changeActiveUser } from "@/services/users.http";
+import { toast } from "react-toastify";
 
 type Input = {
-    isLoadingPage: boolean;
-    onPressModal: (value: number | null) => void;
-    dataFormat: dataFormat[]
-}
+  isLoadingPage: boolean;
+  onPressModal: (value: number | null) => void;
+  dataFormat: tUsuarios[];
+  onSuccess: () => Promise<void>;
+};
 
-const ListUser = ({ isLoadingPage, dataFormat, onPressModal }: Input) => {
+const ListUser = ({
+  isLoadingPage,
+  dataFormat,
+  onPressModal,
+  onSuccess,
+}: Input) => {
+  const openModalEdit = (value: number) => {
+    onPressModal(value);
+  };
 
-    const openModalEdit = (value: number) => {
-        onPressModal(value)
+  const handleAtivo = async (cod: number, is_ativo: boolean) => {
+    const result = await changeActiveUser(cod);
+
+    if (result.success) {
+      toast.success(
+        `Usuário ${is_ativo ? "desativado" : "ativado"} com sucesso`,
+      );
+      await onSuccess();
+    } else {
+      toast.error(result.message);
     }
+  };
+  const handlerOptions = (key: number, isActive: boolean) => {
+    return (
+      <Flex gap={3} justifyContent={"center"} align={"center"}>
+        <IconButton
+          icon={<FaSearch color="gray" size={20} />}
+          onClick={() => openModalEdit(key)}
+        />
 
-    const handerSimNao = (value: 1 | 0) => {
-        return (<>
-            <p
-                style={{
-                    backgroundColor: value ? '#38a169' : '#e53e3e',
-                    borderRadius: "5px",
-                    color: "white",
-                    textAlign: "center",
-                    height: "20px",
-                    paddingRight: "5px",
-                    paddingLeft: "5px",
-                }}>
-                {value ? 'Sim' : 'Não'}
-            </p>
-        </>)
-    }
-    return <>
-
-        {
-            isLoadingPage ?
-                <Stack w={'100%'} bg={'white'} borderRadius={'5px'} p={'10px'} gap={'10px'} boxShadow={'lg'} >
-                    <Loading />
-                </Stack>
-                : <Stack w={'100%'} bg={'white'} borderRadius={'5px'} p={'10px'} gap={'10px'} boxShadow={'lg'} >
-                    <DataGrid
-                        dataSource={dataFormat} keyExpr={"cod"} width="100%" height={"35hw"}
-                        hoverStateEnabled={true} showRowLines={true} allowColumnResizing={true}
-                        columnAutoWidth={true} showBorders={true}>
-                        <Pager
-                            showPageSizeSelector={true}
-                            showNavigationButtons={true}
-                            showInfo={true}
-                            infoText="Total de itens: {2} "
-                            visible={true}
-                        />
-                        <SearchPanel visible={true} />
-                        <Paging defaultPageSize={25} enabled={true} />
-                        <Selection mode="single" />
-                        <Column caption={" - "} dataField={'cod'} alignment={'center'} fixedPosition={"right"} type="buttons" width={"50px"} allowResizing={false}>
-                            <ButtonGrid icon="search" onClick={(e: { row: { data: { cod: number } } }) => { openModalEdit(e.row.data.cod) }} />
-                        </Column>
-                        <Column caption="Criado em." dataField="created_at" minWidth={100} cellRender={(e) => <Text> {dateFormat(e.value, 'dd/MM/yyyy')}</Text>} />
-                        <Column caption="Usuário" dataField="nome" minWidth={110} alignment={'center'} cellRender={(e) => <Text>{e.value}</Text>} />
-                        <Column caption="Login" alignment={'center'} dataField="login" />
-                        <Column caption="CPF" alignment={'center'} dataField="cpf" cellRender={(e) => <Text>{formatCPF(e.value)}</Text>} />
-                        <Column caption="Admin" alignment={'center'} dataField="is_admin" cellRender={(e) => handerSimNao(e.value)} />
-                        <Column caption="Ativo" alignment={'center'} dataField="is_ativo" cellRender={(e) => handerSimNao(e.value)} />
-                        <Column caption="Primeiro Acesso" alignment={'center'} dataField="is_primeiro_acesso" cellRender={(e) => handerSimNao(e.value)} />
-                        
-                    </DataGrid>
-                </Stack>
-        }
-
+        {isActive ? (
+          <IconButton
+            icon={<FaTimesCircle color="#e53e3e" size={20} />}
+            onClick={() => handleAtivo(key, isActive)}
+          />
+        ) : (
+          <IconButton
+            icon={<FaCheckCircle color="#38a169" size={20} />}
+            onClick={() => handleAtivo(key, isActive)}
+          />
+        )}
+      </Flex>
+    );
+  };
+  const handerSimNao = (value: boolean) => {
+    return (
+      <>
+        <p
+          style={{
+            backgroundColor: value ? "#38a169" : "#e53e3e",
+            borderRadius: "5px",
+            color: "white",
+            textAlign: "center",
+            height: "20px",
+            paddingRight: "5px",
+            paddingLeft: "5px",
+          }}
+        >
+          {value ? "Sim" : "Não"}
+        </p>
+      </>
+    );
+  };
+  return (
+    <>
+      {isLoadingPage ? (
+        <Stack
+          w={"100%"}
+          bg={"white"}
+          borderRadius={"5px"}
+          p={"10px"}
+          gap={"10px"}
+          boxShadow={"lg"}
+        >
+          <Loading />
+        </Stack>
+      ) : (
+        <Stack
+          w={"100%"}
+          bg={"white"}
+          borderRadius={"5px"}
+          p={"10px"}
+          gap={"10px"}
+          boxShadow={"lg"}
+        >
+          <DataGrid
+            dataSource={dataFormat}
+            keyExpr={"cod"}
+            width="100%"
+            height={"35hw"}
+            hoverStateEnabled={true}
+            showRowLines={true}
+            allowColumnResizing={true}
+            columnAutoWidth={true}
+            showBorders={true}
+          >
+            <Pager
+              showPageSizeSelector={true}
+              showNavigationButtons={true}
+              showInfo={true}
+              infoText="Total de itens: {2} "
+              visible={true}
+            />
+            <SearchPanel visible={true} />
+            <Paging defaultPageSize={25} enabled={true} />
+            <Selection mode="single" />
+            {/*<Column
+              caption={" - "}
+              dataField={"cod"}
+              alignment={"center"}
+              fixedPosition={"right"}
+              type="buttons"
+              width={"50px"}
+              allowResizing={false}
+            >
+              <ButtonGrid
+                icon="search"
+                onClick={(e: { row: { data: { cod: number } } }) => {
+                  openModalEdit(e.row.data.cod);
+                }}
+              />
+            </Column>*/}
+            <Column
+              caption="#"
+              dataField="#"
+              minWidth={50}
+              alignment={"center"}
+              cellRender={(e) => handlerOptions(e.data.cod, e.data.is_ativo)}
+            />
+            <Column
+              caption="Criado em."
+              dataField="created_at"
+              minWidth={100}
+              cellRender={(e) => (
+                <Text> {dateFormat(e.value, "dd/MM/yyyy")}</Text>
+              )}
+            />
+            <Column
+              caption="Usuário"
+              dataField="nome"
+              minWidth={110}
+              alignment={"center"}
+              cellRender={(e) => <Text>{e.value}</Text>}
+            />
+            <Column caption="Login" alignment={"center"} dataField="login" />
+            {/*<Column
+              caption="CPF"
+              alignment={"center"}
+              dataField="cpf"
+              cellRender={(e) => <Text>{formatCpfCnpj(e.value)}</Text>}
+            />*/}
+            <Column
+              caption="Admin"
+              alignment={"center"}
+              dataField="is_admin"
+              cellRender={(e) => handerSimNao(e.value)}
+            />
+            <Column
+              caption="Ativo"
+              alignment={"center"}
+              dataField="is_ativo"
+              cellRender={(e) => handerSimNao(e.value)}
+            />
+            <Column
+              caption="Primeiro Acesso"
+              alignment={"center"}
+              dataField="is_primeiro_acesso"
+              cellRender={(e) => handerSimNao(e.value)}
+            />
+          </DataGrid>
+        </Stack>
+      )}
     </>
-}
+  );
+};
 
 export default ListUser;
--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,84 +1,115 @@
+import { useEffect, useMemo, useState } from "react";
 import { Button, Flex, Stack, Text } from "@chakra-ui/react";
 import { FaPlus } from "react-icons/fa";
 import ListUser from "../../../components/admin/Usuario/ListUsers";
-import { useEffect, useMemo, useState } from "react";
-import useAuth from "../../../hooks/auth";
 
-import { User } from '../../../types/AuthContextData'
-import ModalUsuario from "../../../components/admin/Usuario/ModalUsuario";
 import { useNavigate } from "react-router";
+import type { tUsuarios } from "@/types/types";
+import { findUserAll } from "@/services/users.http";
+import ModalUsuario from "@/components/admin/Usuario/ModalUsuario";
 
-const UsersAdmin = () => {
+const UsersAdminPage = () => {
+  const router = useNavigate();
 
-    const router = useNavigate();
+  const [userId, setUserId] = useState<null | number>(null);
+  const [isLoadingPage, setIsLoadingPage] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-    const { listUserAdmin } = useAuth();
+  const [dataRequest, setDataRequest] = useState<null | tUsuarios[]>(null);
 
-    const [userId, setUserId] = useState<null | number>(null);
-    const [isLoadingPage, setIsLoadingPage] = useState(false);
-    const [isOpenModal, setIsOpenModal] = useState(false);
+  const onPressModal = (value: number | null = null) => {
+    setIsOpenModal(true);
+    setUserId(value);
+  };
 
-    const [dataRequest, setDataRequest] = useState<null | User[]>(null)
+  const onInit = async () => {
+    setIsLoadingPage(true);
 
-    const onPressModal = (value: number | null = null) => {
-        setIsOpenModal(true);
-        setUserId(value);
+    const result = await findUserAll();
+
+    if (result.success) {
+      setDataRequest(result?.data);
     }
 
-    const onInit = async () => {
-        setIsLoadingPage(true);
+    setTimeout(() => {
+      setIsLoadingPage(false);
+    }, 1500);
+  };
 
-        const result = await listUserAdmin();
+  const dataFormat = useMemo(() => {
+    if (!dataRequest) return [];
 
-        if (result.success) {
-            setDataRequest(result?.data);
-        }
+    return dataRequest;
+  }, [dataRequest]);
 
-        setTimeout(() => {
-            setIsLoadingPage(false);
-        }, 1500)
-    }
-
-    const dataFormat = useMemo(() => {
-        if (!dataRequest) return [];
-
-        return dataRequest?.map((i, index) => {
-            return {
-                index,
-                ...i
-            }
-        })
-    }, [dataRequest])
-
-
-    useEffect(() => {
-        void onInit();
-    }, [])
-    return <>
-        <Flex flexDir={"column"} justifyContent={"center"} alignItems={"flex-start"} gap={'10px'}>
-            <Flex>
-                <Text cursor={'pointer'} onClick={() => router(-1)} _hover={{
-                    textDecoration: 'underline',
-                    color: 'blue'
-                }} > Configurações / Admin / Usuários</Text>
+  useEffect(() => {
+    void onInit();
+  }, []);
+  return (
+    <>
+      <Flex
+        flexDir={"column"}
+        justifyContent={"center"}
+        alignItems={"flex-start"}
+        gap={"10px"}
+      >
+        <Flex>
+          <Text
+            cursor={"pointer"}
+            onClick={() => router(-1)}
+            _hover={{
+              textDecoration: "underline",
+              color: "blue",
+            }}
+          >
+            {" "}
+            Configurações / Admin / Usuários
+          </Text>
+        </Flex>
+        <Stack
+          w={"100%"}
+          bg={"white"}
+          borderRadius={"5px"}
+          p={"10px"}
+          gap={"10px"}
+          boxShadow={"lg"}
+        >
+          <Flex w={"100%"} justify={"space-between"}>
+            <Flex w={"100%"}></Flex>
+            <Flex w={"100%"} justify={"flex-end"}>
+              <Button
+                onClick={() => onPressModal()}
+                isDisabled={isLoadingPage}
+                isLoading={isLoadingPage}
+                colorScheme="green"
+                size={"sm"}
+                leftIcon={<FaPlus />}
+              >
+                {" "}
+                Novo{" "}
+              </Button>
             </Flex>
-            <Stack w={'100%'} bg={'white'} borderRadius={'5px'} p={'10px'} gap={'10px'} boxShadow={'lg'} >
-                <Flex w={'100%'} justify={'space-between'}>
-                    <Flex w={'100%'}>
+          </Flex>
+        </Stack>
 
-                    </Flex>
-                    <Flex w={'100%'} justify={'flex-end'}>
-                        <Button onClick={() => onPressModal()} isDisabled={isLoadingPage} isLoading={isLoadingPage} colorScheme="green" size={'sm'} leftIcon={<FaPlus />}>  Novo </Button>
-                    </Flex>
-                </Flex>
-            </Stack>
-
-            <ListUser dataFormat={dataFormat} isLoadingPage={isLoadingPage} onPressModal={onPressModal} />
-            {
-                isOpenModal && <ModalUsuario onSuccess={onInit} isOpen={isOpenModal} setIsOpenModal={setIsOpenModal} setUserId={setUserId} userId={userId}/>
-            }
-        </Flex >
+        <ListUser
+          dataFormat={dataFormat}
+          isLoadingPage={isLoadingPage}
+          onPressModal={onPressModal}
+          onSuccess={onInit}
+        />
+        {isOpenModal && (
+          <ModalUsuario
+            onSuccess={onInit}
+            isOpen={isOpenModal}
+            setIsOpenModal={setIsOpenModal}
+            setUserId={setUserId}
+            userId={userId}
+          />
+        )}
+      </Flex>
     </>
-}
+  );
+};
 
-export default UsersAdmin;
+export default UsersAdminPage;
